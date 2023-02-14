ee355be01dcef0f1d1b79589176c0f23d7019991
Emily-Erouza <emilysbongile17@gmail.com>
2023-02-14 13:13:07 +0200

    init example

--- index.js
+++ index.js
@@ -13,54 +13,75 @@ let email;
 let password;
 const arr = [];
 const login = [];
+// let id = Maths.floor(Maths.randome() * 20)
 
+var data = localStorage.getItem("login");
 
 const buttonClick = (event) => {
-        event.preventDefault();
-        email = document.getElementById("email").value;
-        password = document.getElementById("password").value;
+    event.preventDefault();
+    email = document.getElementById("email").value;
+    console.log(email);
+    password = document.getElementById("password").value;
 
-        if (email == "") {
-            alert("please fill in email")
-            return
-        }
+    validateEmailAndPassword(password, email);
+};
 
-        if (password == "") {
-            alert("please fil in password")
-            return
-        }
+// console.log('data', data.email)
 
-        if (email.length > 0) {
-            login.push({ " email": email, "password": password })
-            localStorage.setItem("login", JSON.stringify(login));
-        }
-        // var arr = JSON.parse(localStorage.getItem('registerDetails'));
-        // localStorage.setItem("email", email);
-        // localStorage.setItem("password", password);
+// var info = data.findById(id)
 
-        // alert("sucessfully")
+// if (info.email === '' && info.password === "") {
+//     return alert("Your email or password is empty")
 
-        console.log("sucessfully completed")
+// } else {
+//     return "There is something in our email"
+// }
 
+// console.log("email", buttonClick())
 
+// if (password == "") {
+//     return alert("please fill in password!!")
+// } else {
+//     alert("You pressed the wrong password!")
+//     return
+// }
 
+// if (email.length > 0) {
+login.push({ " email": email, password: password });
+localStorage.setItem("login", JSON.stringify(login));
+// }
+// var arr = JSON.parse(localStorage.getItem('registerDetails'));
+// localStorage.setItem("email", email);
+// localStorage.setItem("password", password);
 
+// alert("sucessfully")
+
+// console.log(localStorage.getItem("email"));
+// console.log(localStorage.getItem("password"));
+
+const validateEmailAndPassword = (password, email) => {
+    if (email === "" && password === "") {
+        return alert(" fill in your email and password ");
+    } else if (email === "") {
+        return alert("please enter an email");
+    } else if (password === "") {
+        return alert("please enter an password");
+    } else {
+        return "There is something in our string";
     }
-    // console.log(localStorage.getItem("email"));
-    // console.log(localStorage.getItem("password"));
-
+};
 
 const registerClick = (event) => {
     event.preventDefault();
     email2 = document.getElementById("email2").value;
-    console.log(email);
     password2 = document.getElementById("password2").value;
+    validateEmailAndPassword(password2, email2);
 
-    arr.push({ " email2": email2, "password2": password2 })
+    // arr.push({ " email2": email2, "password2": password2 })
+    // localStorage.setItem("registerDetails", JSON.stringify(arr));
 
-    localStorage.setItem("registerDetails", JSON.stringify(arr));
-    // localStorage.setIterem("password2", password2);
+    // console.log(arr)
+    // return console.log("emza", email2, password2); {
 
-    console.log(arr)
-    return console.log("emza", email2, password2);
-}
+    // }
+};
